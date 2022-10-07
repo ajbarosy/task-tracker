d51344d1891a6e9a2eb3573e02a61e3cc07cea92
Andrew Barosy <102765071+ajbarosy@users.noreply.github.com>
2022-10-07 17:03:45 -0400

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+        <h4>Version 2.0</h4>
+        <a href="/">Go Back</a>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title, onAdd, showAdd}) => {
+
+
+
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button 
+        color={showAdd ? 'red': 'green'} 
+        text={showAdd ? 'Close' : 'Add'} 
+        onClick={onAdd} />
+        
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+//css in JS. if you add style={headingStyle} after h1 tag you can style here
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'purple'
+// }
+
+export default Header
